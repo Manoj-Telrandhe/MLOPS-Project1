a97bbed1e005f8731aa7464050c641822bf5e129
Manoj-Telrandhe <telr.manoj@gmail.com>
2026-02-07 13:34:22 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Manoj Telrandhe",
+    author_email="telr.manoj@gmail.com",
+    packages=find_packages()
+)
